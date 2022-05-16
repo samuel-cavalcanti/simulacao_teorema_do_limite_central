353b3da0b01a7b2f89f9c6c63aedfea7001af231
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-05-15 22:20:48 -0300

    concertado as funções randons

--- sketch.js
+++ sketch.js
@@ -43,41 +43,42 @@ async function draw() {
     const linePosY = 200
     const dy = 150;
     const totalNumbers = 1000
-    const range = [0, 10]
+    const range = [0, 40]
 
 
     const randomOneVariable = (range) => Math.trunc(random(range[0], range[1]));
 
-    const randomTwoVariables = (range) => randomOneVariable(range) + randomOneVariable(range);
+    const randomTwoVariables = (range) => randomOneVariable([range[0], range[1] / 2]) + randomOneVariable([range[0], range[1] / 2]);
 
-    const randomThreeVariables = (range) => randomTwoVariables(range) + randomOneVariable(range);
+    const randomThreeVariables = (range) => randomOneVariable([range[0], range[1] / 3]) + randomOneVariable([range[0], range[1] / 3]) + randomOneVariable([range[0], range[1] / 3]);
 
-    const randomFourVariables = (range) => randomTwoVariables(range) + randomTwoVariables(range);
+    const randomFourVariables = (range) => randomOneVariable([range[0], range[1] / 4]) + randomOneVariable([range[0], range[1] / 4]) + randomOneVariable([range[0], range[1] / 4]) + randomOneVariable([range[0], range[1] / 4]);
 
 
     const oneVariable = generateFrequencyRandomNumbers(totalNumbers, range, randomOneVariable, range[1])
-    const twoVariables = generateFrequencyRandomNumbers(totalNumbers, range, randomTwoVariables, range[1] * 2);
-    const threeVariables = generateFrequencyRandomNumbers(totalNumbers, range, randomThreeVariables, range[1] * 3);
-    const fourVariables = generateFrequencyRandomNumbers(totalNumbers, range, randomFourVariables, range[1] * 4)
+    const twoVariables = generateFrequencyRandomNumbers(totalNumbers, range, randomTwoVariables, range[1]);
+    const threeVariables = generateFrequencyRandomNumbers(totalNumbers, range, randomThreeVariables, range[1]);
+    const fourVariables = generateFrequencyRandomNumbers(totalNumbers, range, randomFourVariables, range[1])
 
 
     textSize(28);
     const title = 'Exemplo prático do teorema de valor central';
+    
 
     text(title, CANVAS_SIZE.width / 2 - textWidth(title) / 2, 50);
 
-
-    text('Uma Variável aleatória', 400, linePosY - 50);
+    const textPosX = 450;
+    text('Uma Variável aleatória', textPosX, linePosY - 50);
     oneVariable.map((value, index) => rect(50 + index * 10, linePosY, 10, - value));
 
 
-    text('Soma de duas variáveis aleatórias', 400, linePosY + dy - 50);
+    text('Soma de duas variáveis aleatórias', textPosX, linePosY + dy - 50);
     twoVariables.map((value, index) => rect(50 + index * 10, linePosY + dy, 10, - value));
 
-    text('Soma de três variáveis aleatórias', 400, linePosY + 2 * dy - 50);
+    text('Soma de três variáveis aleatórias', textPosX, linePosY + 2 * dy - 50);
     threeVariables.map((value, index) => rect(50 + index * 10, linePosY + 2 * dy, 10, - value));
 
-    text('Soma de quatro variáveis aleatórias', 400, linePosY + 3 * dy - 50);
+    text('Soma de quatro variáveis aleatórias', textPosX, linePosY + 3 * dy - 50);
     fourVariables.map((value, index) => rect(50 + index * 10, linePosY + 3 * dy, 10, - value));
 
 
